1bc378adc0e15eafb31f31e5c9ade037d30d4666
Leo Merlo <lmerlo@perceptyx.com>
2022-01-07 11:41:17 -0300

    Chapter 2

---
+++ i18n/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloI18n msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloI18n from "./components/HelloI18n.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloI18n,
+  },
+  created() {
+    const locale = localStorage.getItem('locale');
+
+    if(locale){ 
+      this.$i18n.locale = locale;
+    } else if (navigator.language) {
+      this.$i18n.locale = navigator.language.substring(0, 2);
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ i18n/src/components/HelloI18n.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>{{ $t("hello") }}</p>
+    <p>{{ $t("goodbye") }}</p>
+    <button @click="setLocale('en')">English</button>
+    <button @click="setLocale('es')">Spanish</button>
+    <button @click="setLocale('de')">German</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloI18n",
+  methods: {
+    setLocale(locale) {
+      this.$i18n.locale = locale;
+      localStorage.setItem('locale', locale);
+    }
+  }
+};
+</script>
